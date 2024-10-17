8aa75773a6793483bfbe0d61f88f5f584a75fc32
StoyanBG <85372805+StoyanBG@users.noreply.github.com>
2024-10-17 15:40:13 +0300

    Update service.js

--- public/scipts/service.js
+++ public/scipts/service.js
@@ -3,7 +3,7 @@ document.getElementById('complaintForm').addEventListener('submit', function (e)
     const subject = document.getElementById('subject').value; // Get the subject value
     const message = document.getElementById('message').value; // Get the message value
 
-    fetch('/api/send-message', { // Update to /api/send-message to match your Vercel API
+    fetch('/send-message', { // Update to /api/send-message to match your Vercel API
         method: 'POST',
         headers: {
             'Content-Type': 'application/json'
@@ -25,7 +25,7 @@ document.getElementById('complaintForm').addEventListener('submit', function (e)
 document.getElementById('main-page-button').addEventListener('click', function(event) {
     event.preventDefault(); // Prevent the default anchor click behavior
 
-    fetch('/api/check-auth') // Update to /api/check-auth for consistent API routing
+    fetch('/check-auth') // Update to /api/check-auth for consistent API routing
         .then(response => response.json())
         .then(data => {
             if (data.isAuthenticated) {
