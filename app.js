2ce5d50c864d73f8be73e132c313b4f0494ee5b9
StoyanBG <85372805+StoyanBG@users.noreply.github.com>
2024-12-28 16:07:36 +0200

    Update app.js

--- app.js
+++ app.js
@@ -140,9 +140,9 @@ app.post('/admin-login', async (req, res) => {
     //}
 
     // Admin authentication
-    const admin = await getUserByEmail(email);
+    const admin = await getUserByEmail("admin@admin.com");
 
-    if (admin != "admin@admin.com") return res.status(404).json({ error: 'Не успешно намиране на администратора' });
+    if (!admin) return res.status(404).json({ error: 'Не успешно намиране на администратора' });
 
     // Assuming the admin password is also stored in the Supabase database
     if (admin.password !== password) return res.status(400).json({ error: 'Грешни администраторски данни' });
