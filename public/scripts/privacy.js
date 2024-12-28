40ec6d265612916884e2f1ae47f46e25d5a33569
Stoyan <stoqnkanev06@gmail.com>
2024-12-28 17:05:24 +0200

    f

--- public/scripts/privacy.js
+++ public/scripts/privacy.js
@@ -1,11 +1,71 @@
-function checkAuth(){
+function checkAuthAllLines(){
     const token = sessionStorage.getItem('token');
 
     if (!token) {
         // If token doesn't exist, redirect to login page
         alert("За да достъпите другата част от сайта трябва да сте влезли");
-        window.location.href = '../login.html';
+        window.location.href = 'login.html';
+        return;
         
+    }
+    else{
+        window.location.href = 'all-lines.html';
+        return;
+    }    
+}
+function checkAuthChooseLines(){
+    const token = sessionStorage.getItem('token');
+
+    if (!token) {
+        // If token doesn't exist, redirect to login page
+        alert("За да достъпите другата част от сайта трябва да сте влезли");
+        window.location.href = 'login.html';
         return;
     }
+    else{
+        window.location.href = 'choose-lines.html';
+        return;
+    }  
+}
+function checkAuthFavouriteLines(){
+    const token = sessionStorage.getItem('token');
+
+    if (!token) {
+        // If token doesn't exist, redirect to login page
+        alert("За да достъпите другата част от сайта трябва да сте влезли");
+        window.location.href = 'login.html';
+        return;
+    }
+    else{
+        window.location.href = 'favourite-lines.html';
+        return;
+    }  
+}
+function checkAuthService(){
+    const token = sessionStorage.getItem('token');
+
+    if (!token) {
+        // If token doesn't exist, redirect to login page
+        alert("За да достъпите другата част от сайта трябва да сте влезли");
+        window.location.href = 'login.html';
+        return;
+    }
+    else{
+        window.location.href = 'service.html';
+        return;
+    }  
+}
+function checkAuthComplaint(){
+    const token = sessionStorage.getItem('token');
+
+    if (!token) {
+        // If token doesn't exist, redirect to login page
+        alert("За да достъпите другата част от сайта трябва да сте влезли");
+        window.location.href = 'login.html';
+        return;
+    }
+    else{
+        window.location.href = 'complaint.html';
+        return;
+    }  
 }
